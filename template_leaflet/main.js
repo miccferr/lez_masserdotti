8257e2647755c8ccde2af472d86e50650f16bbd8
Cartowsky <mic.ferretti@gmail.com>
2014-11-21 02:20:51 +0100

    corretto errore in attrib quote

--- template_leaflet/main.js
+++ template_leaflet/main.js
@@ -1,7 +1,7 @@
 // Sostituire MapID con il proprio ID mapbox
 var map = L.map('map').setView([45.4588, 9.2010], 9);
   L.tileLayer('http://{s}.tiles.mapbox.com/v3/MapID/{z}/{x}/{y}.png', {
-    attribution: Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>,
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>',
     maxZoom: 18
 }).addTo(map);
 
